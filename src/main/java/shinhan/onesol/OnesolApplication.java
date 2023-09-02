edcf6519594f5e534bd8e72d1a8c0a09184202ef
dongsuu <suudh99@naver.com>
2023-09-02 21:30:12 +0900

    feat: Add initial settings

---
+++ src/main/java/shinhan/onesol/OnesolApplication.java
@@ -0,0 +1,13 @@
+package shinhan.onesol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnesolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnesolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shinhan/onesol/OnesolApplicationTests.java
@@ -0,0 +1,13 @@
+package shinhan.onesol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnesolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
