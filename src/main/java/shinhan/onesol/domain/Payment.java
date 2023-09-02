95dce88f9c6a11ced42d6ccc3c2c2350f428dbd0
dongsuu <suudh99@naver.com>
2023-09-02 22:59:12 +0900

    feat: Add payment entity

---
+++ src/main/java/shinhan/onesol/domain/Payment.java
@@ -0,0 +1,27 @@
+package shinhan.onesol.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Payment {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+    private int totalPrice;
+
+    @Builder
+    public Payment(Member member, int totalPrice){
+        this.member = member;
+        this.totalPrice = totalPrice;
+    }
+}
