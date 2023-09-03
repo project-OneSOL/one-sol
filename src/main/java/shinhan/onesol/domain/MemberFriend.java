26543522569ed0cd5ab9ceb7846b8638fdcf8a21
dongsuu <suudh99@naver.com>
2023-09-03 23:29:36 +0900

    fix: Fix Member & Friend 연관관계

--- src/main/java/shinhan/onesol/domain/MemberFriend.java
+++ src/main/java/shinhan/onesol/domain/MemberFriend.java
@@ -9,17 +9,18 @@ import javax.persistence.*;
 
 @Entity
 @Getter
+@Table(name = "MEMBER_FRIEND")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberFriend {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToMany
-    @JoinColumn(name = "member_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", referencedColumnName = "member_id")
     private Member member;
 
-    @OneToMany
-    @JoinColumn(name = "member_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "friend_id", referencedColumnName = "member_id")
     private Member friend;
 
     @Builder
