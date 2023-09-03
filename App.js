c8cb891d0725808ddeb927e694bc69e67314d0ec
김현정 <khjung11203@gmail.com>
2023-09-03 23:46:53 +0900

    feat: header

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { Ionicons, Octicons } from "@expo/vector-icons";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.background}>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Image source={require("./assets/1SOL_logo.png")} />
+        <View style={styles.topRight}>
+          <Octicons name="bell-fill" size={24} color="white" />
+          <Ionicons name="person" size={24} color="white" />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#1845FF",
+    paddingHorizontal: 20,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 50,
+  },
+  topRight: {
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 });
