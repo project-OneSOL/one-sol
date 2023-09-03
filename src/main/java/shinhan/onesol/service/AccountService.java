3e6ee920f8580f1f874432920c924d209f440a5a
dongsuu <suudh99@naver.com>
2023-09-03 23:45:37 +0900

    feat: Add account controller & service

---
+++ src/main/java/shinhan/onesol/service/AccountService.java
@@ -0,0 +1,11 @@
+package shinhan.onesol.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AccountService {
+}
