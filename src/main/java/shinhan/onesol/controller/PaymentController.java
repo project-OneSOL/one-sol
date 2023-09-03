3ad878ef9a7e890b4fd7aa79593be9d452485033
dongsuu <suudh99@naver.com>
2023-09-03 23:45:57 +0900

    feat: Add payment controller & service

---
+++ src/main/java/shinhan/onesol/controller/PaymentController.java
@@ -0,0 +1,11 @@
+package shinhan.onesol.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class PaymentController {
+}
