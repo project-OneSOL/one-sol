ccad65aae70b0694b0c264946efe3196f651cc48
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-09-04 23:32:32 +0900

    fix: jwt 기반 인증으로 로직 변경

--- src/main/java/shinhan/onesol/service/MemberService.java
+++ src/main/java/shinhan/onesol/service/MemberService.java
@@ -1,11 +1,27 @@
 package shinhan.onesol.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import shinhan.onesol.dto.TokenInfo;
+import shinhan.onesol.security.JwtTokenProvider;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class MemberService {
+
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Transactional
+    public TokenInfo login(String email, String password) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, password);
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        return jwtTokenProvider.generateToken(authentication);
+    }
+
 }
