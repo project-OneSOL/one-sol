0abec1d86c0c6f31976fb85e87dbe4c11c0d4a3b
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-09-04 23:31:52 +0900

    feat: jwt 토큰 사용 위한 시큐리티 설정 추가 및 변경

--- src/main/java/shinhan/onesol/config/SecurityConfig.java
+++ src/main/java/shinhan/onesol/config/SecurityConfig.java
@@ -9,52 +9,94 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import shinhan.onesol.security.JwtAuthenticationFilter;
 import shinhan.onesol.security.PrincipalService;
 
 @Configuration
-@EnableMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final PrincipalService principalService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    private final String[] PUBLIC_URL = {
+            "/auth/signUp",
+            "/auth/login",
+            "/mail/authentication",
+            "/auth/**",
+            "/oauth2/**",
+            "/login/**",
+    };
+
 
     // TODO 로그인 인증에 상관없이 가져와야 할 페이지들
 
     // 권한에 따라 허용하는 url 설정
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        // /login, /signup 페이지는 모두 허용, 다른 페이지는 인증된 사용자만 허용
+//        http
+//                .authorizeHttpRequests()
+//                .requestMatchers(
+//                        new AntPathRequestMatcher("/auth/login"),
+//                        new AntPathRequestMatcher("/auth/signUp"),
+//                        new AntPathRequestMatcher("/**")
+//                ).permitAll()
+//                .anyRequest().authenticated();
+//
+//        // login 설정
+//        http
+//                .formLogin()
+//                .loginPage("/auth/login")    // GET: 로그인 폼을 보여줌
+//                .usernameParameter("email")    // 로그인에 필요한 아이디
+//                .passwordParameter("password")    // 로그인에 필요한 password 값
+//                .defaultSuccessUrl("/");    // 로그인에 성공하면 /로 redirect
+//        // TODO 소셜로그인 설정
+//
+//        // logout 설정
+//        http
+//                .logout()
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout"))
+//                .logoutSuccessUrl("/")    // 로그아웃에 성공하면 /로 redirect
+//                .invalidateHttpSession(true); // 세션 삭제
+//
+//        http
+//                .userDetailsService(principalService);
+//
+//        return http.build();
+//
+//    }
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // /login, /signup 페이지는 모두 허용, 다른 페이지는 인증된 사용자만 허용
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests()
-                .requestMatchers(
-                        new AntPathRequestMatcher("/**")
-                ).permitAll()
-                .anyRequest().authenticated();
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .sessionManagement()
+                .enableSessionUrlRewriting(true)
+                .and()
 
-        // login 설정
-        http
-                .formLogin()
-                .loginPage("/auth/login")    // GET: 로그인 폼을 보여줌
-                .usernameParameter("email")    // 로그인에 필요한 아이디
-                .passwordParameter("password")    // 로그인에 필요한 password 값
-                .defaultSuccessUrl("/");    // 로그인에 성공하면 /로 redirect
-        // TODO 소셜로그인 설정
+//            .oauth2Login()
+//            .and()
 
-        // logout 설정
-        http
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout"))
-                .logoutSuccessUrl("/")    // 로그아웃에 성공하면 /로 redirect
-                .invalidateHttpSession(true); // 세션 삭제
+                .authorizeRequests()
+                .antMatchers(PUBLIC_URL).permitAll()
+                .antMatchers("/members/user").hasRole("GENERAL")
+                .antMatchers("/members/owner").hasRole("OWNER")
+                .anyRequest().permitAll()
+                .and()
 
-        http
-                .userDetailsService(principalService);
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
-
     }
+
 
     // TODO 비밀번호 찾기 설정 등록
 
