c16ab1ee52058da5f3368adfae87af3f8ea76821
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-09-05 00:13:52 +0900

    chore: 시큐리티 설정 파일 정리

--- src/main/java/shinhan/onesol/config/SecurityConfig.java
+++ src/main/java/shinhan/onesol/config/SecurityConfig.java
@@ -32,45 +32,6 @@ public class SecurityConfig {
             "/login/**",
     };
 
-
-    // TODO 로그인 인증에 상관없이 가져와야 할 페이지들
-
-    // 권한에 따라 허용하는 url 설정
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        // /login, /signup 페이지는 모두 허용, 다른 페이지는 인증된 사용자만 허용
-//        http
-//                .authorizeHttpRequests()
-//                .requestMatchers(
-//                        new AntPathRequestMatcher("/auth/login"),
-//                        new AntPathRequestMatcher("/auth/signUp"),
-//                        new AntPathRequestMatcher("/**")
-//                ).permitAll()
-//                .anyRequest().authenticated();
-//
-//        // login 설정
-//        http
-//                .formLogin()
-//                .loginPage("/auth/login")    // GET: 로그인 폼을 보여줌
-//                .usernameParameter("email")    // 로그인에 필요한 아이디
-//                .passwordParameter("password")    // 로그인에 필요한 password 값
-//                .defaultSuccessUrl("/");    // 로그인에 성공하면 /로 redirect
-//        // TODO 소셜로그인 설정
-//
-//        // logout 설정
-//        http
-//                .logout()
-//                .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout"))
-//                .logoutSuccessUrl("/")    // 로그아웃에 성공하면 /로 redirect
-//                .invalidateHttpSession(true); // 세션 삭제
-//
-//        http
-//                .userDetailsService(principalService);
-//
-//        return http.build();
-//
-//    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -82,8 +43,9 @@ public class SecurityConfig {
                 .enableSessionUrlRewriting(true)
                 .and()
 
-//            .oauth2Login()
-//            .and()
+        // TODO 소셜로그인 설정
+        //            .oauth2Login()
+        //            .and()
 
                 .authorizeRequests()
                 .antMatchers(PUBLIC_URL).permitAll()
@@ -96,7 +58,6 @@ public class SecurityConfig {
 
         return http.build();
     }
-
 
     // TODO 비밀번호 찾기 설정 등록
 
