83a6f62c1f9cb23b90b5b7fd2ee7e91adf79f336
dongsuu <suudh99@naver.com>
2023-09-09 16:53:26 +0900

    feat: SafeArea 적용

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import StackNavigator from "./index.js";
 import { StatusBar } from "expo-status-bar";
 import { palette } from "./lib/styles/colorPalette.js";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
 
 const MyTheme = {
   ...DefaultTheme,
@@ -14,8 +16,22 @@ const MyTheme = {
 export default function App() {
   return (
     <NavigationContainer theme={MyTheme}>
-      <StatusBar style="auto" />
-      <StackNavigator />
+      <SafeAreaView style = {styles.safeArea}>
+        <StatusBar style="auto" />
+        <StackNavigator />  
+      </SafeAreaView>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1, // SafeAreaView를 화면 전체로 확장
+    backgroundColor: palette.main, // 원하는 배경색으로 설정
+  },
+  container: {
+    flex: 1, // 내용을 화면 전체로 확장
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
