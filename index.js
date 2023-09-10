8cb59a6ddeda41396fff73343d91d83ec05888c1
김현정 <khjung11203@gmail.com>
2023-09-10 10:57:57 +0900

    feat: splashscreen

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Header } from "./components/Header";
 import { Home } from "./pages/Home";
 import { palette } from "./lib/styles/colorPalette";
-import { StyleSheet, View, Image } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 import { getHeaderTitle } from "@react-navigation/elements";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
 
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 const Stack = createNativeStackNavigator();
 
 export default function StackNavigator() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        // await Font.loadAsync(Entypo.font);
+        // Artificially delay for two seconds to simulate a slow loading experience.
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately!
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
     <Stack.Navigator
       initialRouteName="Home"
@@ -25,10 +59,11 @@ export default function StackNavigator() {
       //   // ),
       // }}
     >
-      <Stack.Screen 
-        name="Home" 
+      <Stack.Screen
+        name="Home"
         component={Home}
-        options = {{
+        onLayout={onLayoutRootView}
+        options={{
           header: () => <Header />,
           headerStyle: {
             backgroundColor: palette.main,
