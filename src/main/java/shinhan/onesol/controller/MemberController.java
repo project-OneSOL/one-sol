55a797eb1c49cf6287a8a5ab48e27e6202acf096
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-09-10 23:17:25 +0900

    fix: 컨트롤러 로직 서비스 계층으로 분리

--- src/main/java/shinhan/onesol/controller/MemberController.java
+++ src/main/java/shinhan/onesol/controller/MemberController.java
@@ -2,10 +2,41 @@ package shinhan.onesol.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import shinhan.onesol.domain.Member;
+import shinhan.onesol.domain.MemberFriend;
+import shinhan.onesol.repository.MemberFriendRepository;
+import shinhan.onesol.repository.MemberRepository;
+import shinhan.onesol.service.MemberService;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class MemberController {
+
+    private final MemberService memberService;
+
+    // 우선, 검색 탭에서 친구 추가할 멤버 검색
+    // 이메일로 검색
+    @PostMapping("/friend/search")
+    ResponseEntity<Member> searchFriendsByEmail(@RequestParam String email) {
+        return memberService.searchFriendByEmail(email);
+    }
+
+    // 친구 추가 버튼 누를때 친구관계 설정됨
+    @PostMapping("/friend/add")
+    public ResponseEntity<String> addFriend(@RequestParam String requesterEmail, @RequestParam String friendEmail) {
+        return memberService.addFriends(requesterEmail, friendEmail);
+    }
+
+    // 친구 목록 조회
+    @GetMapping("/friend/{memberId}/getList")
+    public ResponseEntity<List<Long>> getFriendList(@PathVariable Long memberId) {
+        return memberService.getFriends(memberId);
+    }
+
 }
