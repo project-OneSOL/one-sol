48408dc7a06d09f5d20f4a0fd57dff923981985e
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-09-10 23:25:44 +0900

    feat: 비밀번호 변경 로직을 위한 구현 준비

---
+++ src/main/java/shinhan/onesol/config/PasswordResetConfig.java
@@ -0,0 +1,17 @@
+//package shinhan.onesol.config;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//public class PasswordResetConfig {
+//
+//    private String passwordResetPage = "config/forgot-password";
+//    private String passwordResetPageUrl = "/config/forgot-password";
+//    private String passwordResetTokenPage = "config/reset-token";
+//    private String passwordResetProcessingPage = "config/reset-password";
+//    private String passwordResetProcessingUrl = "/config/reset-password";
+//    private String passwordResetSuccessUrl = "config/login?reset";
+//
+//}
