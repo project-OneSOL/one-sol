3159877418754c29ba10ca5d5a95c30349510835
김현정 <khjung11203@gmail.com>
2023-09-12 13:44:37 +0900

    feat: TitleContainer 컴포넌트

---
+++ components/TitleContainer.js
@@ -0,0 +1,46 @@
+import { StyleSheet, View } from "react-native";
+import { Title } from "./Title";
+
+export const TitleContainer = (props) => {
+  const { text1, text2, text3 } = props;
+  return (
+    <View style={styles.titleContainer}>
+      <View style={styles.title}>
+        <Title text={text1} size="big"></Title>
+      </View>
+      {text2 && (
+        <View style={styles.subTitle}>
+          <Title text={text2} size="small" weight="light" color="gray"></Title>
+        </View>
+      )}
+      {text3 && (
+        <View style={styles.extraTitle}>
+          <Title text={text3} size="mid" color="blue"></Title>
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    justifyContent: "center",
+    alignContent: "flex-start",
+    marginTop: 20,
+    padding: 20,
+  },
+  title: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingBottom: 5,
+  },
+  subTitle: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  extraTitle: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 20,
+  },
+});
