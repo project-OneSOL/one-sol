989d62f0f52c91cdb4c8e838e2c034f0423ef5fe
dongsuu <suudh99@naver.com>
2023-09-14 03:09:14 +0900

    -feat: paymentMemberState,Dto  추가

---
+++ dtos/request/PaymentMemberDto.js
@@ -0,0 +1,14 @@
+class PaymentMemberDto{
+    constructor(id, name, amount, bankName, cardNumer, cardExpirationYear, cardExpirationMonth, customerIdentityNumber){
+        this.id = id;
+        this.name = name;
+        this.amount = amount;
+        this.bankName = bankName;
+        this.cardNumber = cardNumer;
+        this.cardExpirationYear = cardExpirationYear;
+        this.cardExpirationMonth = cardExpirationMonth;
+        this.customerIdentityNumber = customerIdentityNumber;
+    }
+}
+
+export default PayMember;
