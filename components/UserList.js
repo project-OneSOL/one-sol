ef0970a3bc5508512de09c23b84b4309b46b6143
김현정 <khjung11203@gmail.com>
2023-09-14 09:48:19 +0900

    feat: UserList 컴포넌트

---
+++ components/UserList.js
@@ -0,0 +1,28 @@
+import { StyleSheet, View, ScrollView } from "react-native";
+import { palette } from "../lib/styles/colorPalette";
+import { UserCard } from "./UserCard";
+
+export const UserList = () => {
+  const users = ["이동현", "박기련", "최민수", "김현정"];
+  return (
+    <ScrollView showsVerticalScrollIndicator="false">
+      <View style={styles.friendsList}>
+        {users.map((user) => (
+          <UserCard
+            style={styles.friend}
+            name={user}
+            phone="010-1234-5678"
+          ></UserCard>
+        ))}
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  friendsList: {
+    paddingHorizontal: 6,
+    flexDirection: "column",
+  },
+  friend: {},
+});
