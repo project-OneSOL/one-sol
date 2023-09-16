d22ac060eb393ec1d0b28a9bc515d57b0fbfaed7
김현정 <khjung11203@gmail.com>
2023-09-16 15:48:07 +0900

    feat: navigate, ui

--- app/ScanQR.js
+++ app/ScanQR.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
+import { Button } from "../components/Button";
+import { Title } from "../components/Title";
+import { Background } from "../components/Background";
 
-export const ScanQR = () => {
+export const ScanQR = ({ navigation }) => {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
 
@@ -17,7 +20,8 @@ export const ScanQR = () => {
 
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    // alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    navigation.navigate("DividePay");
   };
 
   if (hasPermission === null) {
@@ -28,22 +32,42 @@ export const ScanQR = () => {
   }
 
   return (
-    <View style={styles.container}>
-      <BarCodeScanner
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={StyleSheet.absoluteFillObject}
-      />
-      {scanned && (
-        <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
-      )}
-    </View>
+    <Background>
+      <View style={styles.container}>
+        <Title text="QR 코드를 스캔해주세요." style={styles.title}></Title>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={[StyleSheet.absoluteFillObject, styles.scan]}
+        />
+        <Button
+          style={styles.btn}
+          title={"다시 스캔하기"}
+          onPress={() => setScanned(false)}
+          disabled={!scanned}
+        />
+      </View>
+    </Background>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 0.9,
     flexDirection: "column",
-    justifyContent: "center",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  title: {
+    // marginTop: 10,
+    // flex: 0.3,
+  },
+  scan: {
+    // flex: 0.5,
+    height: 450,
+    marginTop: 70,
+  },
+  btn: {
+    // flex: 0.3,
+    // marginBottom: 30,
   },
 });
